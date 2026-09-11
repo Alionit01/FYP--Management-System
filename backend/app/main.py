f89b56c2a4dd95306f6304bdf8f1c59d13f9fbcc
Alionit01 <fotopob33@gmail.com>
2026-09-11 05:46:33 +0500

    Connect FastAPI to PostgreSQL

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
 
 
 @app.get("/")
 def root():
     return {"message": "FYP Finder API is running!"}
+
+
+@app.get("/db-test")
+def db_test():
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT 1"))
+        return {"database": result.scalar() == 1}
