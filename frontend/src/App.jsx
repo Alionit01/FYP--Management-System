a7e5ef3f4aa7a6f4d8ee0265b3cdd44fa4281996
Alionit01 <fotopob33@gmail.com>
2026-09-19 04:07:23 +0500

    Add protected frontend routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 import Home from "./pages/Home";
 import Students from "./pages/Students";
 import Teams from "./pages/Teams";
@@ -11,26 +14,48 @@ import CreateTeam from "./pages/CreateTeam";
 import MyTeam from "./pages/MyTeam";
 import MyProfile from "./pages/MyProfile";
 
-
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
 
       <Routes>
+        {/* Public pages */}
         <Route path="/" element={<Home />} />
-
         <Route path="/students" element={<Students />} />
         <Route path="/students/:id" element={<StudentProfile />} />
-        <Route path="/my-profile" element={<MyProfile />} />
-
         <Route path="/teams" element={<Teams />} />
         <Route path="/teams/:id" element={<TeamProfile />} />
-        <Route path="/my-team" element={<MyTeam />} />
-        <Route path="/teams/create" element={<CreateTeam />} />
-        
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
+        {/* Protected pages */}
+        <Route
+          path="/teams/create"
+          element={
+            <ProtectedRoute>
+              <CreateTeam />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/my-team"
+          element={
+            <ProtectedRoute>
+              <MyTeam />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/my-profile"
+          element={
+            <ProtectedRoute>
+              <MyProfile />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
